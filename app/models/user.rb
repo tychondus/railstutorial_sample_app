b913ede0c2920cd4206e0c481b7542dcb787cd3e
tychondus <tychondus@users.noreply.github.com>
2015-09-16 13:47:18 +0800

    added presence and length requirement for passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,11 @@ class User < ActiveRecord::Base
   validates :email, 
             presence: true, 
             length: { maximum: 255 }, 
-            format: {with: VALID_EMAIL_REGEX}, 
+            format: { with: VALID_EMAIL_REGEX }, 
             uniqueness: { case_sensitive: false }
+  validates :password,
+            presence: true,
+            length: { minimum: 6 }
+
   has_secure_password
 end
