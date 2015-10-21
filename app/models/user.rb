20c56396a1f029e787bf73135c254ba257a1c6c1
Sarma <sarma.palaniandy@gmail.com>
2015-10-21 15:16:43 +0800

    login upon signuo

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,9 @@ class User < ActiveRecord::Base
             length: { minimum: 6 }
 
   has_secure_password
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
