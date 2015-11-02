ec5fd7e13f5ac72b9414220ae8643fb9616d77d9
Sarma <sarma.palaniandy@gmail.com>
2015-11-02 12:46:10 +0800

    Added test cases for login without and without rememberingw

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -6,7 +6,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     @user = users(:testuser)
   end
 
-  test " login with invalid information" do
+  test "login with invalid information" do
     get login_path
     assert_template 'sessions/new'
     post login_path, session: { email: "", password: "" }
@@ -58,5 +58,15 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path, count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
+  end
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_not_nil cookies['remember_token']
+  end
+
+  test "login without remembering" do
+    log_in_as(@user, remember_me: '0')
+    assert_nil cookies['remember_token']
   end
 end
